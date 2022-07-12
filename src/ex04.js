3901176df10d846cb30ee7ae1b43fd04f2248e7a
munj1 <yimunj931@gmail.com>
2022-07-12 19:58:27 +0900

    first commit

---
+++ src/ex04.js
@@ -0,0 +1,98 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// ----- 주제: 기본 Geometry 파티클
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+
+  // Mesh
+  const textureLoader = new THREE.TextureLoader();
+  const particleTexture = textureLoader.load("/images/star.png");
+
+  const geometry = new THREE.BufferGeometry(); // buffer임, 직접 채워줘야해
+  const count = 1000; //number of particles
+  const positions = new Float32Array(count * 3);
+  const colors = new Float32Array(count * 3);
+
+  for (let i = 0; i < positions.length; i++) {
+    positions[i] = (Math.random() - 0.5) * 10;
+    colors[i] = Math.random();
+  }
+  geometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(positions, 3) //포인트하나당 값 3개씁니다
+  );
+  geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+
+  console.log(geometry);
+
+  const material = new THREE.PointsMaterial({
+    size: 0.1,
+    map: particleTexture,
+    transparant: true,
+    alphaMap: particleTexture,
+    depthWrite: false,
+    vertexColors: true,
+  });
+  const particles = new THREE.Points(geometry, material);
+  scene.add(particles);
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    controls.update();
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
